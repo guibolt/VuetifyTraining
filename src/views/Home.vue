f87deb57a91e712a357e1fba66d2fc40e89b1f79
guibolt <guilhermedosreiss@gmail.com>
2019-12-06 11:17:28 -0300

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <h1>Home Page</h1>
+    <p class="red white--text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque, laborum nulla alias odio eaque quam numquam necessitatibus, quos, dolores nemo dignissimos pariatur omnis culpa dolorum esse debitis veritatis rem accusamus?</p>
+    <p class="pink lighten-4 red--text text--darken-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, officia ipsam? Molestiae vitae impedit quibusdam tempora necessitatibus quis quae dicta ad. Impedit error nisi temporibus voluptatum. Nisi quam corrupti esse.</p>
+
+    <h1 class="display-3 text-center">Massive Display</h1>
+    <h4 class="display-1 text-center">Smaller display</h4>
+    <p class="headline">This is a headline</p>
+    <p class="subheading font-weight-bold">This is a subheading</p>
+    <p class="caption text-uppercase">this is a caption</p>
+    <p class="caption text-lowercase">this  a caption</p>
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'home'
+ 
+}
+</script>
