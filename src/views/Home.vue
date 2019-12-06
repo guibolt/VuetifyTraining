fc29c3edc31d3ecac778999b23aa382e9afa99f0
guibolt <guilhermedosreiss@gmail.com>
2019-12-06 12:34:25 -0300

    finishing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,30 +3,12 @@
     <h1>This is the homepage</h1>
     <v-divider></v-divider>
 
-    <v-btn class="pink white--text">click me</v-btn>
-    <v-btn depressed color="pink">click me</v-btn>
-    <v-btn flat color="pink">click me</v-btn>
-
-    <v-btn depressed class="pink white--text">
-      <v-icon left>email</v-icon>
-      <span>email me</span>
-    </v-btn>
-
-    <v-btn depressed small class="pink white--text">
-      <v-icon left small>email</v-icon>
-      <span>email me</span>
-    </v-btn>
-
-    <v-btn depressed large class="pink white--text">
-      <span>email me</span>
-      <v-icon right large>email</v-icon>
-    </v-btn>
-
-    <v-btn fab dark small depressed color=" primary">
-      <v-icon dark>favorite</v-icon>
-    </v-btn>
+    <v-btn class="hidden-md-and-down red" >Apenas para gradnes</v-btn>
+    <v-btn class="hidden-md-and-up gren" >Apenas para medios</v-btn>
+    <v-btn class="hidden-sm-only gray">Apenas para pequenos</v-btn>
 
   </div>
+    
 </template>
 
 <script>
